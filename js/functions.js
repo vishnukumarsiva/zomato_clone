5980322def18607b5e84bbcc941ef77411669d83
vishnukumarsiva <vishnukumarsivasubramaniyam@gmail.com>
2022-11-08 01:11:55 +0530

    reworked on responsive and still working on the pages

---
+++ js/functions.js
@@ -0,0 +1,8 @@
+function nav_bar(){
+    var menu_bar_icon =document.getElementById("menu_icon_id");
+    menu_bar_icon.classList.toggle('change');
+    var nb_contents = document.getElementById("sm_nb_s2_id");
+    nb_contents.classList.toggle('change1');
+    var header_container = document.getElementById("header_container_id");
+    header_container.classList.toggle('change2');
+}
---
+++ js/nav_bar.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    var class1 = document.getElementById("menubar");
+    class1.classList.toggle("change");
+   var class2=document.getElementById('nav_bar_page')
+   class2.classList.toggle('change1');
+   var class3=document.getElementById('screen2')
+   class3.classList.toggle('change2');
+   var class4=document.getElementById('s2_nb')
+   class4.classList.toggle('change3');
+
+  }
